e87d406ef2282e86104b712440dd5e357d876c49
Emma-expert <chinweokwu239@gmail.com>
2023-05-14 11:30:53 +0100

    fourth commit adjusted

--- 10-05-2023.py
+++ 10-05-2023.py
@@ -2,14 +2,14 @@ workers = [1, 3, 54, 712, 52, 904, 113, 12, 135, 32, 31, 56, 23, 79, 611, 123, 6
 
 minCost = 0
 for a in range(len(workers)):
-    newWorkers = workers.copy()
-    newWorkers.pop(a)   
-    newWorkers = sorted(newWorkers)
-    newWorkersPair = list(zip(newWorkers[::2], newWorkers[1::2]))
+    currentWorkers = workers.copy()
+    currentWorkers.pop(a)   
+    currentWorkers = sorted(currentWorkers)
+    newWorkersPair = list(zip(currentWorkers[::2], currentWorkers[1::2]))
     cost = sum([abs(i-j) for (i,j) in newWorkersPair])
     if a == 0:
         minCost = cost
     elif cost < minCost:
         minCost = cost
 
-print("Minimum cost of array of efficiencies is", minCost)
+print("The minimum cost of array of efficiencies:", minCost)
